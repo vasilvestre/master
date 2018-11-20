0585bbd7c15fec4e56f7f8141cc0d30e874765d8
Valentin Silvestre <vasilvestre@laposte.net>
2018-11-20 22:04:03 +0100

    bishop move

--- java/chess/src/main/java/catho/pieces/Bishop.java
+++ java/chess/src/main/java/catho/pieces/Bishop.java
@@ -1,9 +1,26 @@
 package catho.pieces;
 
+import catho.Coordinate;
 import catho.Piece;
 
 public abstract class Bishop extends Piece {
     public Bishop(String name, String representation, Integer number) {
         super(name, representation, number);
+    }
+
+    @Override
+    public boolean canMove(Coordinate fromCoordinate, Coordinate toCoordinate) {
+        Integer distanceBetweenVertical;
+        Integer distanceBetweenHorizontal;
+
+        distanceBetweenVertical = toCoordinate.getVerticalAxis() - fromCoordinate.getVerticalAxis();
+        distanceBetweenHorizontal = Integer.parseInt(toCoordinate.getHorizontalAxis()) - Integer.parseInt(fromCoordinate.getHorizontalAxis());
+
+        if (moveToDifferentPosition(fromCoordinate, toCoordinate)
+                && distanceBetweenVertical.equals(distanceBetweenHorizontal)
+        ) {
+            return true;
+        }
+        return false;
     }
 }
