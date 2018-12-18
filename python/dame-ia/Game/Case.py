4fd3888052d2a53f0d53f5529fe6f8764fcc481c
Valentin Silvestre <vasilvestre@laposte.net>
2018-12-18 13:17:30 +0100

    remove old dir

--- python/dame-ia/Game/Case.py
+++
@@ -1,8 +0,0 @@
-class Case(object):
-    position = 0
-    color = ''
-    dame = False
-
-    def __init__(self, position):
-        self.position = position
-
--- python/dame-ia/Game/Player.py
+++
@@ -1,5 +0,0 @@
-class Player(object):
-    color = ''
-
-    def __init__(self, color):
-        self.color = color
