c4c45117c14c31bdc60c24299b06e0371a7a4250
Valentin Silvestre <vasilvestre@laposte.net>
2019-09-17 09:21:34 +0200

    Update fun

---
+++ big_data/uk_sales/import.sql
@@ -0,0 +1,12 @@
+CREATE TABLE uk_sales
+(
+    InvoiceNo int NOT NULL,
+    StockerCode text,
+    Description text,
+    Quantity int,
+    InvoiceDate date,
+    UnitPrice float,
+    CustomerId int,
+    Country TEXT
+);
+
